01ee5dfd64b09b3ee49157a5867831169469054b
Nadir <fobasip@gmail.com>
2018-05-05 22:41:44 +0300

    Update sdo.js
    color highlights

--- Some small scripts/sdo.js
+++ Some small scripts/sdo.js
@@ -1,2 +1,20 @@
-javascript: var elements = document.body.querySelectorAll('.hm-subject-list-item-description-lessons');
-Array.prototype.forEach.call(elements, function(el, i) { el.setAttribute('style', 'height: 100% !important;'); });
+javascript: var elements = document.body.querySelectorAll(
+  ".hm-subject-list-item-description-lessons"
+);
+Array.prototype.forEach.call(elements, function(el, i) {
+  el.setAttribute("style", "height: 100% !important;");
+
+  $(el).filter((index, element) => {
+    var text = $(element)
+      .find(".lesson-status-tab")
+      .each(function(index) {
+        if (
+          $(this)
+            .text()
+            .indexOf("(Выдано задание) 0") != -1
+        ) {
+          $(this).css({ background: "yellow" });
+        }
+      });
+  });
+});
